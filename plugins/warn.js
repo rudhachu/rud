d0a0e54a66a11a2710169e4f014b762a63490694
rudhachu <120007671+rudhachu@users.noreply.github.com>
2024-11-11 11:39:46 +0530

    Update warn.js

--- plugins/warn.js
+++ plugins/warn.js
@@ -1,74 +1,92 @@
-const { rudhra, isAdmin ,parsedJid, mode } = require("../lib");
-const { PausedChats, WarnDB, saveWarn, resetWarn } = require("../lib");
-const { WARN_COUNT } = require("../config");
-const config = require('../config');
-
-const checkPermissions = async (message) => {
-    if (message.isSudo) return true;
-    if (!config.ADMIN_ACCESS) return false;
-    return await message.isAdmin(message.sender);
-};
-
+const {
+    rudhra,
+    getWarn,
+    isAdmin,
+    config
+} = require('../lib');
 
 rudhra({
-    pattern: "warn",
-    fromMe: false,
-    onlyGroup: true,
-    desc: "Warn a user",
+    pattern: "warn ?(.*)",
+    fromMe: true,
+    onlyGroup: true
     type: "group",
-  },
-async (message, match) => {
-    if (!(await checkPermissions(message))) return;
-    const userId = message.mention[0] || message.reply_message.jid;
-    if (!userId) return message.reply("_Mention or reply to someone_");
-    let isadmin = await isAdmin(message.jid, message.user, message.client);
-    if (!isadmin) return
-    let reason = message?.reply_message.text || match;
-    reason = reason.replace(/@(\d+)/, "");
-    reason = reason ? reason.length <= 1 : "Reason not Provided";
-
-    const warnInfo = await saveWarn(userId, reason);
-    let userWarnCount = warnInfo ? warnInfo.warnCount : 0;
-    userWarnCount++;
-    await message.reply(
-      `     *⚠️ WARNING  ⚠️*\nUser @${userId.split("@")[0]}  \nWarn Count: ${userWarnCount}. \nReason: ${reason}`,
-      { mentions: [userId] }
-    );
-    if (userWarnCount > WARN_COUNT) {
-      const jid = parsedJid(userId);
-      await message.sendMessage(
-        message.jid,
-        "Warn limit exceeded kicking user"
-      );
-      return await message.client.groupParticipantsUpdate(
-        message.jid,
-        jid,
-        "remove"
-      );
-    }
-    return;
-  }
-);
-
-rudhra({
-    pattern: "resetwarn",
-    fromMe: false,
-    onlyGroup: true,
-    desc: "Reset warnings for a user",
-    type: "group",
-  },
-async (message, match) => {
-    if (!(await checkPermissions(message))) return;
-    const userId = message.mention[0] || message.reply_message.jid;
-    if (!userId) return message.reply("_Mention or reply to someone_");
-    let isadmin = await isAdmin(message.jid, message.user, message.client);
-    if (!isadmin) return
-    await resetWarn(userId);
-    return await message.reply(
-      `*Reset Warning For* @${userId.split("@")[0]}`,
-      {
-        mentions: [userId],
-      }
-    );
-  }
-);
+    desc: "warn a user in group",
+}, async (message, match) => {
+    if (!match && !message.reply_message.sender) return await message.reply("_Example:- warn get_\n_warn 9091808100@g.us <reply>\nwarn remove 9091808100@s.whatsapp.net_");
+    if (match == 'get') {
+        const {
+            warn
+        } = await getWarn(['warn'], {
+            jid: message.jid,
+            content: {}
+        }, 'get');
+        if (!Object.keys(warn)[0]) return await message.reply('_Not Found!_');
+        let msg = '';
+        for (const f in warn) {
+            msg += `_*User:* @${f}_\n_*Count:* ${warn[f].count}_\n_*Remaining:* ${config.WARN_COUNT - warn[f].count}`;
+        }
+        return await message.send(msg, {mentions: [message.reply_message.sender], quoted: message });
+    } else if (match == 'reset') {
+        if (!message.reply_message.sender) return await message.reply("_Please Reply To a user_");
+        const {
+            warn
+        } = await getWarn(['warn'], {
+            jid: message.jid,
+            content: {}
+        }, 'get');
+        if (!Object.keys(warn)[0]) return await message.reply('_Not Found!_');
+        if (!Object.keys(warn).includes(message.reply_message.number)) return await message.reply('_User Not Found!_');
+        await getWarn(['warn'], {
+            jid: message.jid,
+            content: {
+                id: message.reply_message.number
+            }
+        }, 'delete');
+        return await message.reply('_Warn reset Successfully_');
+    } else {
+        const admin = await isAdmin(message);
+        if (!admin) return await message.reply(*_I'm not admin._*);
+        if (config.ADMIN_SUDO_ACCESS != 'true' && !message.isCreator) return await message.reply("_requst faild with status code 501_");
+        if (!admin && !message.isCreator) return await message.reply("_requst faild with status code 501_");
+        if (!message.reply_message.sender) return await message.reply("_Please Reply To a user_");
+        const reason = match || 'warning';
+        const {
+            warn
+        } = await getWarn(['warn'], {
+            jid: message.jid,
+            content: {}
+        }, 'get');
+        const count = Object.keys(warn).includes(message.reply_message.number) ? Number(warn[message.reply_message.number].count) + 1 : 1;
+        await getWarn(['warn'], {
+                jid: message.jid,
+                content: {
+                    [message.reply_message.number]: {
+                        count
+                    }
+                }
+            },
+            'add');
+        const remains = config.WARN_COUNT - count;
+                let warnmsg = `╭─❏ ❮ *ᴡᴀʀɴɪɴɢ* ❯ ❏
+│ _*ᴜsᴇʀ : @${message.reply_message.number}⁩*_
+│ _*ᴡᴀʀɴ : ${count}*_
+│ _*ʀᴇᴀsᴏɴ : ${reason}*_
+│ _*ʀᴇᴍᴀɪɴɪɴɢ : ${remains}*_
+╰─❏`
+        await message.send(warnmsg, {
+            mentions: [message.reply_message.sender], quoted: message
+        })
+        if (remains <= 0) {
+            await getWarn(['warn'], {
+                jid: message.jid,
+                content: {
+                    id: message.reply_message.number
+                }
+            }, 'delete');
+            if (admin) {
+                await message.client.groupParticipantsUpdate(message.from, [message.reply_message.sender], 'remove');
+                return await message.reply("_Max warns reached, kicked out!_"
+            };
+        };
+    };
+})
