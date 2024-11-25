24be13b077b879eb4d8e0c8a4c85e20cef23ae5f
rudhachu <120007671+rudhachu@users.noreply.github.com>
2024-11-25 13:12:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,8 +21,8 @@ DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBU
 module.exports = {
   BRANCH: "main",
   ADMIN_ACCESS: toBool(process.env.ADMIN_ACCESS) || false,
-  ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
-  ANTILINK: toBool(process.env.ANTI_LINK) || true,
+  ANTILINK_ACTION: process.env.ANTILINK_ACTION || "kick",
+  ANTI_LINK: toBool(process.env.ANTI_LINK) || true,
   AUTHOR: process.env.AUTHOR || "Ƥ ʀ ɪ ɴ ᴄ ᴇ  Ʀ ᴜ ᴅ ʜ",
   AUDIO_DATA: process.env.AUDIO_DATA || "Ʀ ᴜ ᴅ ʜ ʀ λ;Ƥ ʀ ɪ ɴ ᴄ ᴇ  Ʀ ᴜ ᴅ ʜ;https://raw.githubusercontent.com/rudhra-prh/media/main/image/rudhra2.jpg",
   BOT_NAME: process.env.BOT_NAME || "Ʀᴜᴅʜʀᴀ ʙᴏᴛ",
